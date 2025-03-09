26f5ddfa18c149ce000cb0b29bc9a1261b3c741c
svdragster <svdragster@gmail.com>
2025-03-09 21:23:26 +0100

    fps AK with animation and spray

---
+++ src/fps_gun_plugin.rs
@@ -0,0 +1,292 @@
+use crate::FpsControllerSetup;
+use bevy::pbr::NotShadowCaster;
+use bevy::prelude::*;
+use bevy::render::camera::Exposure;
+use bevy::render::view::RenderLayers;
+use bevy::scene::SceneInstanceReady;
+use bevy_fps_controller::controller::{CameraConfig, FpsController, LogicalPlayer, RenderPlayer};
+use bevy_rapier3d::geometry::Collider;
+use std::f32::consts::{PI, TAU};
+use std::time::Duration;
+use bevy::animation::RepeatAnimation;
+
+pub struct FpsGunPlugin;
+
+impl Plugin for FpsGunPlugin {
+    fn build(&self, app: &mut App) {
+        app.add_systems(Startup, (setup.after(FpsControllerSetup),));
+        app.add_systems(Update, (move_listener, on_fps_gun_animation));
+    }
+}
+
+#[derive(Component, Clone, Debug, Eq, PartialEq, Hash)]
+pub struct GunAnimationState {
+    pub walking: bool,
+    pub shooting: bool,
+    pub reloading: bool,
+    pub previous_state: Option<Box<GunAnimationState>>,
+}
+/// Used by the view model camera and the player's arm.
+/// The light source belongs to both layers.
+const VIEW_MODEL_RENDER_LAYER: usize = 1;
+
+#[derive(Component)]
+pub struct ViewModelRenderPlayer;
+
+#[derive(Component, Clone)]
+pub struct FpsGunAnimationsData {
+    pub default_animation_index: usize,
+    pub animations: Vec<AnimationNodeIndex>,
+    pub graph: Handle<AnimationGraph>,
+    pub current_animation_index: usize,
+}
+
+#[derive(Copy, Clone, Debug, Eq, PartialEq, Hash)]
+pub enum GunAnimations {
+    Idle = 0,
+    Shooting = 1,
+    Walking = 2,
+    //Reloading = 3,
+}
+
+impl Default for GunAnimations {
+    fn default() -> Self {
+        GunAnimations::Idle
+    }
+}
+
+impl GunAnimations {
+    fn all() -> Vec<GunAnimations> {
+        vec![
+            GunAnimations::Idle,
+            GunAnimations::Shooting,
+            GunAnimations::Walking,
+        ]
+    }
+
+    fn all_indices() -> Vec<usize> {
+        Self::all().into_iter().map(|anim| anim as usize).collect()
+    }
+
+    fn get_speed(&self) -> f32 {
+        match self {
+            GunAnimations::Idle => 1.0,
+            GunAnimations::Walking => 1.0,
+            GunAnimations::Shooting => 2.5,
+        }
+    }
+}
+
+#[derive(Component, Clone)]
+pub struct LastPosition {
+    pub last_position: Vec3,
+}
+
+fn setup(
+    mut commands: Commands,
+    asset_server: Res<AssetServer>,
+    mut graphs: ResMut<Assets<AnimationGraph>>,
+) {
+    commands.spawn((
+        ViewModelRenderPlayer,
+        Camera3d::default(),
+        Camera {
+            // Bump the order to render on top of the world model.
+            order: 1,
+            ..default()
+        },
+        Projection::from(PerspectiveProjection {
+            fov: 80.0_f32.to_radians(),
+            ..default()
+        }),
+        // Only render objects belonging to the view model.
+        RenderLayers::layer(VIEW_MODEL_RENDER_LAYER),
+    ));
+
+    // Spawn the gun
+    spawn_gun(
+        &mut commands,
+        &asset_server,
+        &mut graphs,
+        "ak47_animated.glb",
+    );
+
+    commands.spawn((
+        DirectionalLight {
+            illuminance: light_consts::lux::AMBIENT_DAYLIGHT,
+            shadows_enabled: true,
+            ..default()
+        },
+        Transform {
+            translation: Vec3::new(0.0, 2.0, 0.0),
+            rotation: Quat::from_rotation_x(-PI / 4.),
+            ..default()
+        },
+        // The light source illuminates both the world model and the view model.
+        RenderLayers::from_layers(&[VIEW_MODEL_RENDER_LAYER]),
+    ));
+}
+
+fn spawn_gun(
+    commands: &mut Commands,
+    asset_server: &Res<AssetServer>,
+    graphs: &mut ResMut<Assets<AnimationGraph>>,
+    asset_path: &str,
+) {
+    let (graph, node_indices) = AnimationGraph::from_clips(
+        GunAnimations::all_indices()
+            .iter()
+            .map(|&index| {
+                asset_server
+                    .load(GltfAssetLabel::Animation(index).from_asset(asset_path.to_string()))
+            })
+            .collect::<Vec<_>>(),
+    );
+
+    let graph_handle = graphs.add(graph);
+    let animations = FpsGunAnimationsData {
+        default_animation_index: GunAnimations::Idle as usize,
+        animations: node_indices,
+        graph: graph_handle,
+        current_animation_index: GunAnimations::Idle as usize,
+    };
+
+    let scene = asset_server.load(GltfAssetLabel::Scene(0).from_asset(asset_path.to_string()));
+    commands
+        .spawn((
+            SceneRoot(scene),
+            Transform {
+                translation: Vec3::new(1.0, -1.0, -1.5),
+                scale: Vec3::splat(0.15),
+                rotation: Quat::from_euler(EulerRot::XYX, 0.0, -PI, 0.0),
+            },
+            RenderLayers::from_layers(&[VIEW_MODEL_RENDER_LAYER]),
+            animations,
+        ))
+        .observe(on_gun_loaded);
+}
+
+fn on_gun_loaded(
+    trigger: Trigger<SceneInstanceReady>,
+    mut commands: Commands,
+    children_query: Query<&Children>,
+    animations: Query<&FpsGunAnimationsData>,
+    mut players: Query<&mut AnimationPlayer>,
+    children: Query<&Children>,
+) {
+    let entity = trigger.entity();
+    let scene_instance_entity = children.get(trigger.entity()).unwrap()[0];
+    let entity_to_animate = children.get(scene_instance_entity).unwrap()[0];
+    let animations = animations.get(trigger.entity()).unwrap();
+    for child in children_query.iter_descendants(entity) {
+        commands.entity(child).log_components();
+        commands.entity(child).insert((
+            // Ensure the gun is only rendered by the view model camera.
+            RenderLayers::layer(VIEW_MODEL_RENDER_LAYER),
+            // The gun is free-floating, so shadows would look weird.
+            NotShadowCaster,
+        ));
+    }
+
+    let mut animation_player = players.get_mut(entity_to_animate).unwrap();
+    let mut transitions = AnimationTransitions::new();
+
+    transitions
+        .play(
+            &mut animation_player,
+            animations.animations[animations.default_animation_index],
+            Duration::ZERO,
+        )
+        .repeat();
+
+    animation_player.adjust_speeds(1.0);
+
+    commands.entity(entity_to_animate).insert((
+        AnimationGraphHandle(animations.graph.clone()),
+        transitions,
+        GunAnimationState {
+            walking: false,
+            shooting: false,
+            reloading: false,
+            previous_state: Some(
+                GunAnimationState {
+                    walking: false,
+                    shooting: false,
+                    reloading: false,
+                    previous_state: None,
+                }
+                .into(),
+            ),
+        },
+    ));
+}
+
+fn move_listener(
+    mut player_query: Query<(Entity, &Transform, &mut LastPosition), With<LogicalPlayer>>,
+    mut gun_animation_state: Query<&mut GunAnimationState>,
+) {
+    let (player_entity, transform, mut last_position) = player_query.get_single_mut().unwrap();
+    let current_position = transform.translation;
+    let delta = current_position - last_position.last_position;
+    last_position.last_position = current_position;
+    let mut animation = GunAnimations::Idle;
+    if let Ok(mut gun_animation_state) = gun_animation_state.get_single_mut() {
+        if delta.length_squared() > 0.01 * 0.01 {
+            gun_animation_state.walking = true;
+        } else {
+            gun_animation_state.walking = false;
+        }
+    }
+}
+
+fn on_fps_gun_animation(
+    mut animation_query: Query<(
+        &mut AnimationPlayer,
+        &mut AnimationTransitions,
+        &mut GunAnimationState,
+    )>,
+    mut animations: Query<&mut FpsGunAnimationsData>,
+) {
+    if let Ok((mut animation_player, mut transitions, mut state)) = animation_query.get_single_mut()
+    {
+        let previous_state = state.previous_state.clone();
+        let mut animations = animations.get_single_mut().unwrap();
+        let mut duration = 0;
+        if let Some(previous_state) = previous_state {
+            let mut new_animation: Option<GunAnimations> = None;
+            if state.shooting {
+                if !previous_state.shooting {
+                    new_animation = Some(GunAnimations::Shooting);
+                    duration = 100;
+                }
+            } else if state.walking {
+                if !previous_state.walking
+                    || animations.current_animation_index == GunAnimations::Shooting as usize
+                {
+                    new_animation = Some(GunAnimations::Walking);
+                    duration = 200;
+                }
+            } else if !state.walking && !state.shooting {
+                new_animation = Some(GunAnimations::Idle);
+                duration = 200;
+            }
+            if let Some(new_animation) = new_animation {
+                if animations.current_animation_index != new_animation as usize {
+                    // Idle animation
+                    transitions
+                        .play(
+                            &mut animation_player,
+                            animations.animations[new_animation as usize],
+                            Duration::from_millis(duration),
+                        )
+                        .repeat();
+                    for (index, active_animation) in animation_player.playing_animations_mut() {
+                        active_animation.set_speed(new_animation.get_speed());
+                    }
+                    animations.current_animation_index = new_animation as usize;
+                }
+            }
+        }
+        state.previous_state = Some(Box::new(state.clone()));
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,22 @@
+mod fps_gun_plugin;
+
+use crate::fps_gun_plugin::FpsGunPlugin;
 use bevy::prelude::*;
 use bevy::render::camera::Exposure;
+use bevy::render::view::RenderLayers;
+use bevy::time::Stopwatch;
 use bevy::window::CursorGrabMode;
 use bevy_fps_controller::controller::*;
 use bevy_rapier3d::prelude::*;
-use std::f32::consts::TAU;
-use bevy::render::view::RenderLayers;
-
 use bevy_rapier3d::prelude::*;
-use rand::Rng;
+use rand::prelude::*;
+use std::f32::consts::TAU;
+use rand::distr::Uniform;
 
 const SPAWN_POINT: Vec3 = Vec3::new(0.0, 1.625, 0.0);
+
+#[derive(SystemSet, Debug, Hash, PartialEq, Eq, Clone)]
+pub struct FpsControllerSetup;
 
 #[derive(Debug, Clone, Default, Component, Reflect)]
 #[reflect(Component, Default)]
@@ -21,6 +28,17 @@ struct PointsDisplay;
 #[derive(Default, Resource)]
 struct Points {
     pub value: i32,
+}
+
+#[derive(Component)]
+struct ShootTracker {
+    stopwatch: Stopwatch,
+    spray: usize,
+}
+
+#[derive(Component)]
+struct BulletImpact {
+    stopwatch: Stopwatch,
 }
 
 fn main() {
@@ -35,31 +53,19 @@ fn main() {
         .add_plugins(RapierPhysicsPlugin::<NoUserData>::default())
         // .add_plugins(RapierDebugRenderPlugin::default())
         .add_plugins(FpsControllerPlugin)
-        .add_systems(Startup, setup)
-        .add_systems(Update, (respawn, manage_cursor, click_targets, update_points_display)) // Add update_points_display system
+        .add_plugins(FpsGunPlugin)
+        .add_systems(
+            Startup,
+            (setup, fps_controller_setup.in_set(FpsControllerSetup)),
+        )
+        .add_systems(
+            Update,
+            (respawn, manage_cursor, click_targets, update_points_display, despawn_bullet_impacts),
+        ) // Add update_points_display system
         .run();
 }
 
-fn setup(
-    mut commands: Commands,
-    mut window: Query<&mut Window>,
-    assets: Res<AssetServer>,
-    mut meshes: ResMut<Assets<Mesh>>,
-    mut materials: ResMut<Assets<StandardMaterial>>,
-    mut materials2d: ResMut<Assets<ColorMaterial>>,
-) {
-    let mut window = window.single_mut();
-    window.title = String::from("Minimal FPS Controller Example");
-
-    commands.spawn((
-        DirectionalLight {
-            illuminance: light_consts::lux::FULL_DAYLIGHT,
-            shadows_enabled: true,
-            ..default()
-        },
-        Transform::from_xyz(4.0, 14.0, 5.0).looking_at(Vec3::ZERO, Vec3::Y),
-    ));
-
+fn fps_controller_setup(mut commands: Commands, asset_server: Res<AssetServer>) {
     let height = 3.0;
     let logical_entity = commands
         .spawn((
@@ -99,11 +105,21 @@ fn setup(
         .insert(CameraConfig {
             height_offset: -0.5,
         })
+        .insert(fps_gun_plugin::LastPosition {
+            last_position: Vec3::ZERO,
+        })
+        .insert(ShootTracker {
+            stopwatch: Stopwatch::new(),
+            spray: 0,
+        })
         .id();
 
     commands.spawn((
         Camera3d::default(),
-        Camera { order: 0, ..default() },
+        Camera {
+            order: 0,
+            ..default()
+        },
         Projection::Perspective(PerspectiveProjection {
             fov: TAU / 5.0,
             ..default()
@@ -111,7 +127,35 @@ fn setup(
         Exposure::SUNLIGHT,
         RenderPlayer { logical_entity },
     ));
-    commands.spawn((Camera2d, Camera { order: 1, ..default() }));
+}
+
+fn setup(
+    mut commands: Commands,
+    mut window: Query<&mut Window>,
+    assets: Res<AssetServer>,
+    mut meshes: ResMut<Assets<Mesh>>,
+    mut materials: ResMut<Assets<StandardMaterial>>,
+    mut materials2d: ResMut<Assets<ColorMaterial>>,
+) {
+    let mut window = window.single_mut();
+    window.title = String::from("Minimal FPS Controller Example");
+
+    commands.spawn((
+        DirectionalLight {
+            illuminance: light_consts::lux::FULL_DAYLIGHT,
+            shadows_enabled: true,
+            ..default()
+        },
+        Transform::from_xyz(4.0, 14.0, 5.0).looking_at(Vec3::ZERO, Vec3::Y),
+    ));
+
+    commands.spawn((
+        Camera2d,
+        Camera {
+            order: 2,
+            ..default()
+        },
+    ));
 
     // Ground collider
     commands.spawn((
@@ -126,8 +170,20 @@ fn setup(
     });
     commands.spawn((
         Mesh3d(meshes.add(Cuboid::new(40.0, 0.1, 40.0))),
-        MeshMaterial3d(ground_material),
+        MeshMaterial3d(ground_material.clone()),
         Transform::from_translation(Vec3::new(0.0, -0.5, 0.0)),
+    ));
+
+    // Wall
+    commands.spawn((
+        Collider::cuboid(5.0, 2.5, 0.5),
+        RigidBody::Fixed,
+        Transform::from_translation(Vec3::new(0.0, 0.0, 10.0)),
+    ));
+    commands.spawn((
+        Mesh3d(meshes.add(Cuboid::new(10.0, 5.0, 1.0))),
+        MeshMaterial3d(ground_material.clone()),
+        Transform::from_translation(Vec3::new(0.0, 0.0, 10.0)),
     ));
 
     spawn_random_target(&mut commands, &mut meshes, &mut materials);
@@ -139,11 +195,7 @@ fn setup(
     commands.spawn((
         Mesh2d(meshes.add(Circle::new(2.0))),
         MeshMaterial2d(materials2d.add(color)),
-        Transform::from_xyz(
-            0.0,
-            0.0,
-            0.0,
-        ),
+        Transform::from_xyz(0.0, 0.0, 0.0),
     ));
 
     commands.spawn((
@@ -155,10 +207,9 @@ fn setup(
             bottom: Val::Px(5.0),
             left: Val::Px(15.0),
             ..default()
-        },PointsDisplay
+        },
+        PointsDisplay,
     ));
-
-
 }
 
 fn respawn(mut query: Query<(&mut Transform, &mut Velocity)>) {
@@ -196,7 +247,20 @@ fn manage_cursor(
     }
 }
 
-
+const SPRAY_DIRECTIONS: [Vec3; 12] = [
+    Vec3::new(0.0, 0.0, 0.0),
+    Vec3::new(-0.01, 0.025, 0.0),
+    Vec3::new(-0.02, 0.05, 0.0),
+    Vec3::new(-0.03, 0.055, 0.0),
+    Vec3::new(-0.032, 0.065, 0.0),
+    Vec3::new(-0.034, 0.075, 0.0),
+    Vec3::new(-0.038, 0.08, 0.0),
+    Vec3::new(-0.042, 0.082, 0.0),
+    Vec3::new(-0.046, 0.085, 0.0),
+    Vec3::new(-0.042, 0.087, 0.0),
+    Vec3::new(-0.039, 0.090, 0.0),
+    Vec3::new(-0.038, 0.093, 0.0),
+];
 
 fn click_targets(
     mut commands: Commands,
@@ -207,38 +271,86 @@ fn click_targets(
     camera: Query<&Transform, With<RenderPlayer>>,
     buttons: Res<ButtonInput<MouseButton>>,
     targets: Query<Entity, With<Target>>,
-    mut points: ResMut<Points>, // Add this line
+    mut points: ResMut<Points>,
+    mut gun_animation_state: Query<&mut fps_gun_plugin::GunAnimationState>,
+    mut shoot_stopwatch: Query<&mut ShootTracker>,
+    time: Res<Time>,
 ) {
-    if buttons.just_pressed(MouseButton::Left) {
-        let rapier_context = rapier_context.single();
-        let player_handle = player_query.single();
-        let camera_transform = camera.single();
-        let ray_pos = camera_transform.translation;
-        let ray_dir = camera_transform.forward().as_vec3();
-        let max_toi: bevy_rapier3d::math::Real = 100.0;
-        let solid = true;
-        let filter = QueryFilter::new()
-          .exclude_sensors()
-          .exclude_rigid_body(player_handle);
+    let player_handle = player_query.single();
+    let mut shoot_tracker = shoot_stopwatch
+        .get_mut(player_handle)
+        .expect("LogicalPlayer also needs a ShootStopwatch");
+    shoot_tracker.stopwatch.tick(time.delta());
 
-        if let Some((entity, toi)) = rapier_context.cast_ray(ray_pos, ray_dir, max_toi, solid, filter) {
-            // Handle the hit.
-            if let Ok(target_entity) = targets.get(entity) {
-                println!("Hit target entity {:?}", target_entity);
-                // Remove the target
-                commands.entity(entity).despawn_recursive();
-                // Spawn a new target
-                spawn_random_target(&mut commands, &mut meshes, &mut materials);
-                // Increment points
-                points.value += 1;
+    if let Ok(mut gun_animation_state) = gun_animation_state.get_single_mut() {
+        if buttons.pressed(MouseButton::Left) {
+            gun_animation_state.shooting = true;
+        } else {
+            gun_animation_state.shooting = false;
+        }
+    }
+    if buttons.pressed(MouseButton::Left) {
+        if shoot_tracker.stopwatch.elapsed_secs() > 0.1 {
+            shoot_tracker.stopwatch.reset();
+            let rapier_context = rapier_context.single();
+            let camera_transform = camera.single();
+            let ray_pos = camera_transform.translation;
+            let spray: Vec3;
+            if shoot_tracker.spray >= SPRAY_DIRECTIONS.len() {
+                let mut rng = rand::rng();
+                let range = Uniform::new(-0.1f32, 0.1).unwrap();
+                spray = Vec3::new(
+                    rng.sample(range),
+                    rng.sample(range),
+                    0.0,
+                );
+            } else {
+                spray = SPRAY_DIRECTIONS[shoot_tracker.spray];
+            }
+            shoot_tracker.spray += 1;
+            let ray_dir = camera_transform.forward().as_vec3() + camera_transform.rotation * spray;
+            let max_toi: bevy_rapier3d::math::Real = 100.0;
+            let solid = true;
+            let filter = QueryFilter::new()
+                .exclude_sensors()
+                .exclude_rigid_body(player_handle);
+
+            if let Some((entity, toi)) =
+                rapier_context.cast_ray(ray_pos, ray_dir, max_toi, solid, filter)
+            {
+                let hit_point = ray_pos + ray_dir * Vec3::splat(toi.into());
+                println!("Hit entity {:?} at {:?}", entity, hit_point);
+                commands.spawn((
+                    BulletImpact {
+                        stopwatch: Stopwatch::new(),
+                    },
+                    Transform::from_translation(hit_point),
+                    Mesh3d(meshes.add(Sphere::new(0.1))),
+                    MeshMaterial3d(materials.add(StandardMaterial {
+                        base_color: Color::srgb(1.0, 0.0, 0.0),
+                        ..Default::default()
+                    })),
+                  ));
+
+
+                // Handle the hit.
+                if let Ok(target_entity) = targets.get(entity) {
+                    println!("Hit target entity {:?}", target_entity);
+                    // Remove the target
+                    commands.entity(entity).despawn_recursive();
+                    // Spawn a new target
+                    spawn_random_target(&mut commands, &mut meshes, &mut materials);
+                    // Increment points
+                    points.value += 1;
+                } else {
+                    points.value -= 1;
+                }
             } else {
                 points.value -= 1;
             }
         }
-        else {
-            points.value -= 1;
-        }
-        
+    } else {
+        shoot_tracker.spray = 0;
     }
 }
 
@@ -269,11 +381,21 @@ fn spawn_random_target(
     ));
 }
 
-fn update_points_display(
-    points: Res<Points>,
-    mut query: Query<&mut Text, With<PointsDisplay>>,
-) {
+fn update_points_display(points: Res<Points>, mut query: Query<&mut Text, With<PointsDisplay>>) {
     for mut text in &mut query {
         text.0 = format!("Points: {}", points.value);
+    }
+}
+
+fn despawn_bullet_impacts(
+    mut commands: Commands,
+    mut bullet_impacts: Query<(Entity, &mut BulletImpact)>,
+    time: Res<Time>,
+) {
+    for (entity, mut impact) in &mut bullet_impacts.iter_mut() {
+        impact.stopwatch.tick(time.delta());
+        if impact.stopwatch.elapsed_secs() > 0.1 {
+            commands.entity(entity).despawn_recursive();
+        }
     }
 }
